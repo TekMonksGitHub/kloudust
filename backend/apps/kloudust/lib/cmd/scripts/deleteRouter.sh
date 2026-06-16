d33d1095d5ea19293cb801ad8ea9ba99aeeed0bc
Siddharth Tiwari <siddharth.tiwari@deeplogictech.com>
2026-06-16 12:54:35 +0530

    [Updated] Delete router to clear boot script

--- backend/apps/kloudust/lib/cmd/scripts/deleteRouter.sh
+++ backend/apps/kloudust/lib/cmd/scripts/deleteRouter.sh
@@ -5,6 +5,8 @@
 
 NAME="{1}"
 
+ROUTER_BOOT_SCRIPT="/kloudust/system/hostinit/30router_${ROUTER_NAME}.sh"
+
 function exitFailed() {
     echo Failed
     exit 1
@@ -12,5 +14,6 @@ function exitFailed() {
 
 if ! ip netns delete $NAME; then exitFailed; fi
 
+rm $ROUTER_BOOT_SCRIPT
 printf "\n\nRouter $NAME deleted successfully"
 exit 0
