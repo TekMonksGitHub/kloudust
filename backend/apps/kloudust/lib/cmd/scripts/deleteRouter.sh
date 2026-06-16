b50cc6958963bc25041d8d43cac99804e8b304db
Siddharth Tiwari <siddharth.tiwari@deeplogictech.com>
2026-06-16 13:12:22 +0530

    [Update] Fixed Variable Name

--- backend/apps/kloudust/lib/cmd/scripts/deleteRouter.sh
+++ backend/apps/kloudust/lib/cmd/scripts/deleteRouter.sh
@@ -3,7 +3,7 @@
 # Params
 # {1}Router Name - no spaces
 
-NAME="{1}"
+ROUTER_NAME="{1}"
 
 ROUTER_BOOT_SCRIPT="/kloudust/system/hostinit/30router_${ROUTER_NAME}.sh"
 
@@ -12,8 +12,8 @@ function exitFailed() {
     exit 1
 }
 
-if ! ip netns delete $NAME; then exitFailed; fi
+if ! ip netns delete $ROUTER_NAME; then exitFailed; fi
 
 rm $ROUTER_BOOT_SCRIPT
-printf "\n\nRouter $NAME deleted successfully"
+printf "\n\nRouter $ROUTER_NAME deleted successfully"
 exit 0
